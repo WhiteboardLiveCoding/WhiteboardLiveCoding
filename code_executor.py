192d0d08eb0c3952cfa84cef225af69614636792
Martin Zlocha <mz4315@ic.ac.uk>
2017-10-10 09:51:46 +0100

    Allow code in strings to be executed and prints results

--- code_executor.py
+++ code_executor.py
@@ -1,3 +1,29 @@
+import sys
+import io
+
+
 class CodeExecutor:
     def execute_code(self, code):
-        pass
+        print("Executing code: \n{}\n".format(code))
+
+        code_out = io.StringIO()
+        code_err = io.StringIO()
+
+        sys.stdout = code_out
+        sys.stderr = code_err
+
+        exec(code)
+
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        s = code_err.getvalue()
+
+        print("Error:\n{}\n".format(s))
+
+        s = code_out.getvalue()
+
+        print("Output:\n{}\n".format(s))
+
+        code_out.close()
+        code_err.close()
