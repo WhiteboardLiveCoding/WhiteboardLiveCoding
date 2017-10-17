38afe779ada2ac678fac806721ffbe2f1e04af0f
rr3015 <richard.rohac15@imperial.ac.uk>
2017-10-17 15:28:28 +0100

    Code now executes in the container

--- WLC/code_executor.py
+++ WLC/code_executor.py
@@ -1,6 +1,7 @@
 import io
 import logging
 import sys
+import docker
 
 LOGGER = logging.getLogger()
 
@@ -15,15 +16,23 @@ class CodeExecutor:
         with io.StringIO() as code_out:
             sys.stdout = code_out
 
-            try:
-                exec(code)
-            except:
-                LOGGER.exception("An error was raised!")
-            else:
-                LOGGER.info("No errors occurred.")
+            self.execute_code_docker(code, docker_ip)
 
-            sys.stdout = sys.__stdout__
+            # try:
+            #     exec(code)
+            # except:
+            #     LOGGER.exception("An error was raised!")
+            # else:
+            #     LOGGER.info("No errors occurred.")
+            #
+            # sys.stdout = sys.__stdout__
+            #
+            # s = code_out.getvalue()
+            #
+            # LOGGER.info("Output:\n%s\n", s)
 
-            s = code_out.getvalue()
+    def execute_code_docker(self, code, docker_ip):
+        client = docker.DockerClient(base_url="tcp://{}:2375".format(docker_ip))
+        LOGGER.info("\n\n  # Docker Version running on server: %s\n", client.version()["Version"])
 
-            LOGGER.info("Output:\n%s\n", s)
+        client.containers.run('python', 'python -c \"{}\"'.format(code))
