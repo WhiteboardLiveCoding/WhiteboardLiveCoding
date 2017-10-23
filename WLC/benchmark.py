6e149e978f2bc5b031ca2ad0072f642bb80912f1
Martin Zlocha <mz4315@ic.ac.uk>
2017-10-23 23:32:24 +0100

    Separate fixed and non-fixed accuracy in benchmark

--- WLC/benchmark.py
+++ WLC/benchmark.py
@@ -30,13 +30,16 @@ def benchmark_file(file_name):
     code = code.lower()
     fixed_code = CodeFixer(code, indents).fix()
 
-    difference = editdistance.eval("".join(fixed_code.split()), "".join(expected_code.split()))
+    difference = editdistance.eval("".join(code.split()), "".join(expected_code.split()))
+    difference_fixed = editdistance.eval("".join(fixed_code.split()), "".join(expected_code.split()))
 
     length = len("".join(expected_code.split()))
     accuracy = round(100 - (difference * 100 / length))
+    accuracy_fixed = round(100 - (difference_fixed * 100 / length))
 
-    LOGGER.info('Accuracy: {}%, File: {}'.format(accuracy, file_name))
-    return accuracy, length
+    LOGGER.info('Accuracy w/o fixing: {}%, Accuracy w/ fixing: {}%, Fix improvement: {}%,  File: {}'.format(
+        accuracy, accuracy_fixed, accuracy_fixed - accuracy,file_name))
+    return accuracy, accuracy_fixed, length
 
 
 def run_benchmarks():
@@ -45,20 +48,25 @@ def run_benchmarks():
     LOGGER.info('')
 
     total_accuracy = 0
+    total_accuracy_fixed = 0
     total_length = 0
 
     for i in range(1, 9):
-        accuracy, length = benchmark_file('assets/examples/images/example_{}.png'.format(i))
+        accuracy, accuracy_fixed, length = benchmark_file('assets/examples/images/example_{}.png'.format(i))
         total_accuracy += accuracy * length
+        total_accuracy_fixed += accuracy_fixed * length
         total_length += length
 
     overall_accuracy = round(total_accuracy / total_length)
+    overall_accuracy_fixed = round(total_accuracy_fixed / total_length)
 
     LOGGER.info('')
-    LOGGER.info('Overall Accuracy: {}%'.format(overall_accuracy))
+    LOGGER.info('Overall Accuracy w/o fix: {}%'.format(overall_accuracy))
+    LOGGER.info('Overall Accuracy w/ fix: {}%'.format(overall_accuracy_fixed))
+    LOGGER.info('Fix improvement: {}%'.format(overall_accuracy_fixed - overall_accuracy))
     LOGGER.info('Code length: {}'.format(total_length))
 
-    return overall_accuracy
+    return overall_accuracy_fixed
 
 
 if __name__ == '__main__':
