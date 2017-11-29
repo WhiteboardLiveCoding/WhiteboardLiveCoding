ff95c6e10fc3673d94367819d96374be328e04c9
Paul Larsen <pn1315@ic.ac.uk>
2017-11-29 16:00:46 +0000

    Add arguments to the benchmark to allow us to run individual testsuites

--- WLC/benchmark.py
+++ WLC/benchmark.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import re
 import os
@@ -13,6 +14,16 @@ import editdistance
 
 logging.basicConfig(format=FORMAT)
 LOGGER = logging.getLogger()
+
+
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--language", default="all", help="Choose which language to run tests for. Defaults to \
+                        all languages")
+
+    args, unknown = parser.parse_known_args()
+
+    return args.language
 
 
 def _get_expected_code(file_name):
@@ -44,7 +55,7 @@ def benchmark_file(file_name, language="python3"):
     return accuracy, accuracy_fixed, length
 
 
-def run_benchmarks():
+def run_benchmarks(language="all"):
     LOGGER.info('=== Whiteboard Live Coding Benchmarking ===')
     LOGGER.info('Uses Levenshtein distance to calculate the difference and then uses that to calculate accuracy.')
 
@@ -52,31 +63,34 @@ def run_benchmarks():
     total_accuracy_fixed = 0
     total_length = 0
 
-    LOGGER.info('')
-    LOGGER.info('Testing Python3 code:')
-    python_directory = get_full_path('assets/examples/images/python3/')
+    if language.lower() == "python3" or language.lower() == "all":
+        LOGGER.info('')
+        LOGGER.info('Testing Python3 code:')
+        python_directory = get_full_path('assets/examples/images/python3/')
 
-    for file in (f for f in os.listdir(python_directory) if isfile(join(python_directory, f)) and not f.startswith(".")):
-        file_path = join(python_directory, file)
-        accuracy, accuracy_fixed, length = benchmark_file(file_path, "python3")
+        for file in (f for f in os.listdir(python_directory) if isfile(join(python_directory, f)) and not f.startswith(".")):
+            file_path = join(python_directory, file)
+            accuracy, accuracy_fixed, length = benchmark_file(file_path, "python3")
 
-        total_accuracy += accuracy * length
-        total_accuracy_fixed += accuracy_fixed * length
-        total_length += length
+            total_accuracy += accuracy * length
+            total_accuracy_fixed += accuracy_fixed * length
+            total_length += length
 
-    LOGGER.info('')
-    LOGGER.info('Testing Haskell code:')
-    haskell_directory = get_full_path('assets/examples/images/haskell/')
-    for file in (f for f in os.listdir(haskell_directory) if isfile(join(haskell_directory, f)) and not f.startswith(".")):
-        file_path = join(haskell_directory, file)
-        accuracy, accuracy_fixed, length = benchmark_file(file_path, "haskell")
+    if language.lower() == "haskell" or language.lower() == "all":
+        LOGGER.info('')
+        LOGGER.info('Testing Haskell code:')
+        haskell_directory = get_full_path('assets/examples/images/haskell/')
+        for file in (f for f in os.listdir(haskell_directory) if
+                     isfile(join(haskell_directory, f)) and not f.startswith(".")):
+            file_path = join(haskell_directory, file)
+            accuracy, accuracy_fixed, length = benchmark_file(file_path, "haskell")
 
-        total_accuracy += accuracy * length
-        total_accuracy_fixed += accuracy_fixed * length
-        total_length += length
+            total_accuracy += accuracy * length
+            total_accuracy_fixed += accuracy_fixed * length
+            total_length += length
 
-    overall_accuracy = round(total_accuracy / total_length, 2)
-    overall_accuracy_fixed = round(total_accuracy_fixed / total_length, 2)
+    overall_accuracy = round(total_accuracy / total_length, 2) if total_length else 0
+    overall_accuracy_fixed = round(total_accuracy_fixed / total_length, 2) if total_length else 0
 
     LOGGER.info('')
     LOGGER.info('Overall Accuracy w/o fix: %s%%', overall_accuracy)
@@ -88,5 +102,6 @@ def run_benchmarks():
 
 
 if __name__ == '__main__':
+    language = arguments()
     LOGGER.setLevel(logging.INFO)
-    run_benchmarks()
+    run_benchmarks(language)
