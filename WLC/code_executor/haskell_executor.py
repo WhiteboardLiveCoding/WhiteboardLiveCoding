d8fb753889f7b1f0addf9cf1f3fe558554f7c1d5
Martin Zlocha <mz4315@ic.ac.uk>
2017-11-29 14:00:31 +0000

    Implement haskell executing and error reporting.

--- WLC/code_executor/haskell_executor.py
+++ WLC/code_executor/haskell_executor.py
@@ -1,3 +1,9 @@
+import re
+import tempfile
+
+import os
+from subprocess import Popen, PIPE
+
 from ..code_fixing.python_code_fixer import PythonCodeFixer
 from ..code_executor.abstract_executor import AbstractCodeExecutor
 
@@ -7,10 +13,25 @@ class HaskellExecutor(AbstractCodeExecutor):
         super().__init__("haskell", PythonCodeFixer, ip, port)
 
     def execute_local(self, code):
-        raise NotImplemented()
+        file_code = tempfile.NamedTemporaryFile(delete=False, suffix='.hs')
+        file_code.write(code.encode('utf8'))
+        file_code.close()
+
+        proc = Popen('runghc {}'.format(file_code.name), stdout=PIPE, stderr=PIPE)
+        out, err = proc.communicate()
+
+        os.unlink(file_code.name)
+
+        return out.decode("utf-8"), self._get_code_errors(err.decode("utf-8"))
 
     def execute_sandbox(self, code):
         raise NotImplemented()
 
-    def get_code_errors(self, code):
-        raise NotImplemented()
+    def _get_code_errors(self, err):
+        matches = re.findall('hs:(\d+):(\d+): (\w+):\W+([^\r\n]+)', err, flags=re.M)
+        errors = []
+
+        for error in matches:
+            errors.append({'line': int(error[0]), 'column': int(error[1]), 'type': error[2], 'message': error[3]})
+
+        return errors
